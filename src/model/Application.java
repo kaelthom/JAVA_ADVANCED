3e1a849201164878bca956d9b8cb98c0ea9d9540
Mickael Thomere <mickael.thomere@gmail.com>
2020-05-26 09:51:00 +0200

    Inner classes : Parking exercise

--- src/model/Application.java
+++ src/model/Application.java
@@ -1,23 +1,32 @@
 package model;
 
 public class Application {
+
+    public static final String TOTAL_COST_LABEL = " vehicles, unregistered total cost is ";
+    public static final String PEUGEOT = "Peugeot";
+    public static final String ASTRA = "Astra";
+
     public static void main(String[] args) {
-        Parking<Vehicle> newParking = new Parking<>("A3", "Standard");
-        newParking.add(new RegisteredCar("Peugeot"));
-        newParking.add(new UnregisteredCar("Astra"));
+        Parking<Vehicle> newParking = new Parking<>("JJP", "Jean Jaures Public");
+        newParking.add(new RegisteredCar(PEUGEOT));
+        newParking.add(new UnregisteredCar(ASTRA));
         newParking.add(new UnregisteredTruck("Renault"));
         newParking.add(new RegisteredTruck("Ford"));
         newParking.add(new UnregisteredTruck("Subaru"));
-        System.out.println(newParking.getVehicles().size() + " vehicles, unregistered total cost is " + newParking.calculateTotalPrice());
+        System.out.println(newParking.getVehicles().size() + TOTAL_COST_LABEL + newParking.calculateTotalPrice());
 
-        Parking<Car> newCarParking = new Parking<>("A3", "Only Cars");
-        newCarParking.add(new RegisteredCar("Peugeot"));
+        Parking<Car> newCarParking = new Parking<>("JJC", "Jean Jaures Car");
+        newCarParking.add(new RegisteredCar(PEUGEOT));
         newCarParking.add(new UnregisteredCar("Astra"));
-        System.out.println(newCarParking.getVehicles().size() + " vehicles, unregistered total cost is " + newCarParking.calculateTotalPrice());
+        System.out.println(newCarParking.getVehicles().size() + TOTAL_COST_LABEL + newCarParking.calculateTotalPrice());
 
-        Parking<Truck> newTruckParking = new Parking<>("A3", "Only Truck");
-        newTruckParking.add(new RegisteredTruck("Peugeot"));
+        Parking<Truck> newTruckParking = new Parking<>("JJT", "Jean Jaures Truck");
+        newTruckParking.add(new RegisteredTruck(PEUGEOT));
         newTruckParking.add(new UnregisteredTruck("Astra"));
-        System.out.println(newTruckParking.getVehicles().size() + " vehicles, unregistered total cost is " + newTruckParking.calculateTotalPrice());
+        System.out.println(newTruckParking.getVehicles().size() + TOTAL_COST_LABEL + newTruckParking.calculateTotalPrice());
+
+        System.out.println(newParking.getGates());
+        System.out.println(newCarParking.getGates());
+        System.out.println(newTruckParking.getGates());
     }
 }
--- src/model/Parking.java
+++ src/model/Parking.java
@@ -6,7 +6,8 @@ import java.util.stream.IntStream;
 
 public class Parking<T extends Vehicle> {
     private final int capacity = 15;
-    List<T> vehicles;
+    private List<T> vehicles;
+    private List<TollGate> tollGates;
     private String code;
     private String name;
 
@@ -14,10 +15,23 @@ public class Parking<T extends Vehicle> {
         this.code = code;
         this.name = name;
         this.vehicles = new ArrayList<>();
+        this.tollGates = new ArrayList<>();
+
+        tollGates.add(new TollGate(Direction.IN, Orientation.NORTH));
+        tollGates.add(new TollGate(Direction.OUT, Orientation.SOUTH));
     }
 
     public List<T> getVehicles() {
         return vehicles;
+    }
+
+    public String getGates() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.name).append(" gates:\n");
+        for (TollGate tollGate : tollGates) {
+            sb.append("--Gate: ").append(tollGate.toString()).append("\n");
+        }
+        return sb.toString();
     }
 
     public void add(T vehicle) {
@@ -29,5 +43,40 @@ public class Parking<T extends Vehicle> {
                 .filter(vehicle -> vehicle instanceof TollGatePayable)
                 .flatMapToInt(vehicle -> IntStream.of(((TollGatePayable) vehicle).payTollGate()))
                 .sum();
+    }
+
+    public enum Direction {
+        IN,
+        OUT,
+        INOUT
+    }
+
+    public enum Orientation {
+        NORTH,
+        SOUTH,
+        WEST,
+        EAST
+    }
+
+    public class TollGate {
+        private int id;
+        private boolean enable;
+        private Direction direction;
+        private Orientation orientation;
+        private Parking parking;
+
+        public TollGate(Direction direction, Orientation orientation) {
+            this.enable = true;
+            this.direction = direction;
+            this.orientation = orientation;
+            this.parking = Parking.this;
+            this.id = this.parking.tollGates.size();
+        }
+
+        @Override
+        public String toString() {
+            return this.parking.code + "#door-" + id;
+        }
+
     }
 }
