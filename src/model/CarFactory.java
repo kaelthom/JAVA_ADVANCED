36d2f4b1240f7da6a6f1a90a379b967b4afbb86f
Mickael Thomere <mickael.thomere@gmail.com>
2020-05-27 11:37:24 +0200

    Conception and Design Patterns : Abstract Factory - improve

--- src/model/CarFactory.java
+++ src/model/CarFactory.java
@@ -1,10 +1,12 @@
 package model;
 
 public class CarFactory extends AbstractVehicleFactory {
+    @Override
     public Vehicle createUnregisteredVehicle(String brand, String plate) {
         return new UnregisteredCar(brand, plate);
     }
 
+    @Override
     public Vehicle createRegisteredVehicle(String brand, String plate) {
         return new RegisteredCar(brand, plate);
     }
--- src/model/TruckFactory.java
+++ src/model/TruckFactory.java
@@ -1,10 +1,12 @@
 package model;
 
 public class TruckFactory extends AbstractVehicleFactory {
+    @Override
     public Vehicle createUnregisteredVehicle(String brand, String plate) {
         return new UnregisteredTruck(brand, plate);
     }
 
+    @Override
     public Vehicle createRegisteredVehicle(String brand, String plate) {
         return new RegisteredTruck(brand, plate);
     }
